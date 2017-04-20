9abe05e4fcdf34172a53d629fcd015fd08e058f3
PrzemyslawSz <przemyslszczecina@gmail.com>
2017-04-20 22:33:36 +0200

    Twoj komentarz

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def Omnie
+ end
+ def kontakt
+ end
+
 end
---
+++ db/migrate/20170420193130_add_user_id_to_pins.rb
@@ -0,0 +1,4 @@
+class AddUserIdToPins < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
